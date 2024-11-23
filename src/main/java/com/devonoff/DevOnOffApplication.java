dee741fdfa833b6018e50e00528fd18b1e5b648b
SwanyCastle <seunghwan712@gmail.com>
2024-11-23 20:15:58 +0900

    Init: 프로젝트 초기 설정 및 .gitignore, PR 템플릿 추가

---
+++ src/main/java/com/devonoff/DevOnOffApplication.java
@@ -0,0 +1,13 @@
+package com.devonoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOnOffApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevOnOffApplication.class, args);
+  }
+
+}
