b5ab3b3ad45dbefedd1102b930ee4b9bdb4504c7
heekuukuu <gmlrud2di@gmail.com>
2024-11-25 23:30:11 +0900

    fix:Pr(UserDetails 상속)

--- src/main/java/com/devonoff/entity/User.java
+++ src/main/java/com/devonoff/entity/User.java
@@ -11,10 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 @Getter
@@ -23,7 +27,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,7 +48,6 @@ public class User {
   private LoginType loginType = LoginType.GENERAL; // 로그인 타입 (GENERAL, GOOGLE, NAVER 등)
 
 
-
   @Column(nullable = true, name = "profile_image_url")
   private String profileImage; // 기본 역할
 
@@ -62,5 +65,29 @@ public class User {
     this.updatedAt = LocalDateTime.now();
   }
 
+  // UserDetails 인터페이스 기본 구현
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptyList(); // 현재 권한 관리 제외
+  }
 
+  @Override
+  public boolean isAccountNonExpired() {
+    return true; // 계정 만료 여부
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true; // 계정 잠김 여부
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true; // 자격 증명 만료 여부
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return this.isActive; // 활성 상태
+  }
 }
