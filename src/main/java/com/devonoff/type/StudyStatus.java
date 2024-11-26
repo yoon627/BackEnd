521f1935a349ed583392189760e4d429c3c03371
hjkim <hjhjgudwls1022@gmail.com>
2024-11-26 18:47:57 +0900

    feat: 모집 취소 및 자동 삭제 로직 구현 (Spring Batch 및 Schedule 적용)

--- src/main/java/com/devonoff/type/StudyStatus.java
+++ src/main/java/com/devonoff/type/StudyStatus.java
@@ -1,7 +1,8 @@
 package com.devonoff.type;
 
 public enum StudyStatus {
-  RECRUITING,   // 모집 중
-  IN_PROGRESS,  // 진행 중
-  CANCELED      // 모집 취소
+  RECRUITING,         // 모집 중
+  IN_PROGRESS,        // 진행 중
+  CANCELED,           // 모집 취소
+  DELETION_SCHEDULED  // 삭제 예약
 }
