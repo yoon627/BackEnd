9049b059ef08cb9108c20fe0033cafbc4ee4b489
hjkim <hjhjgudwls1022@gmail.com>
2024-11-26 21:00:39 +0900

    feat: QueryDSL 설정 및 검색 리스트 조회 서비스 로직 구현

--- src/main/java/com/devonoff/studyPost/repository/StudyPostRepository.java
+++ src/main/java/com/devonoff/studyPost/repository/StudyPostRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudyPostRepository extends JpaRepository<StudyPost, Long> {
+public interface StudyPostRepository extends JpaRepository<StudyPost, Long>,
+    StudyPostRepositoryCustom {
 
   void deleteByStatusAndUpdatedAtBefore(StudyStatus status, LocalDateTime dateTime);
 }
