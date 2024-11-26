0945075a626acf56d5fff6caf2eedd64887cb2e6
hjkim <hjhjgudwls1022@gmail.com>
2024-11-26 15:09:57 +0900

    feat: 스터디 모집글 생성 서비스 로직 구현

---
+++ src/main/java/com/devonoff/studyPost/service/StudyPostService.java
@@ -0,0 +1,55 @@
+package com.devonoff.studyPost.service;
+
+import com.devonoff.exception.CustomException;
+import com.devonoff.studyPost.dto.StudyPostCreateDto;
+import com.devonoff.studyPost.dto.StudyPostCreateDto.Request;
+import com.devonoff.studyPost.entity.StudyPost;
+import com.devonoff.studyPost.repository.StudyPostRepository;
+import com.devonoff.type.ErrorCode;
+import com.devonoff.type.StudyStatus;
+import com.devonoff.user.entity.User;
+import com.devonoff.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudyPostService {
+
+  private final StudyPostRepository studyPostRepository;
+  private final UserRepository userRepository;
+
+  public StudyPostCreateDto.Response createStudyPost(StudyPostCreateDto.Request request) {
+    User user = userRepository.findById(request.getUserId())
+        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+    StudyPost studyPost = buildStudyPost(request, user);
+
+    studyPostRepository.save(studyPost);
+
+    return new StudyPostCreateDto.Response("스터디 모집 글이 생성되었습니다.");
+  }
+
+  // TODO: 엔티티로 이동시킬지 고려
+  private static StudyPost buildStudyPost(Request request, User user) {
+    return StudyPost.builder()
+        .title(request.getTitle())
+        .studyName(request.getStudyName())
+        .subject(request.getSubject())
+        .difficulty(request.getDifficulty())
+        .dayType(request.getDayType())
+        .startDate(request.getStartDate())
+        .endDate(request.getEndDate())
+        .startTime(request.getStartTime())
+        .endTime(request.getEndTime())
+        .meetingType(request.getMeetingType())
+        .recruitmentPeriod(request.getRecruitmentPeriod())
+        .description(request.getDescription())
+        .latitude(request.getLatitude())
+        .longitude(request.getLongitude())
+        .status(StudyStatus.RECRUITING) // 기본값: 모집 중
+        .thumbnailImgUrl(request.getThumbnailImgUrl())
+        .user(user)
+        .build();
+  }
+}
