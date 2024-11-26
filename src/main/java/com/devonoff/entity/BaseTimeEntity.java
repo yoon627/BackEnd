c2cc1f2e46aba0230b617422389786bc4be61577
hjkim <hjhjgudwls1022@gmail.com>
2024-11-26 14:20:06 +0900

    feat: 공통 시간 관리를 위한 BaseTimeEntity 생성

---
+++ src/main/java/com/devonoff/entity/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.devonoff.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+  @CreatedDate
+  @Column(nullable = false) // 생성될때만 실행, 이 후 변경하지 않음
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
