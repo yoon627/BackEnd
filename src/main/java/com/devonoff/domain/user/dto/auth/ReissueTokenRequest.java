a54e7effd127fba5632de8d4596e52c3bff9d90e
SwanyCastle <seunghwan712@gmail.com>
2024-11-28 15:03:54 +0900

    feat: 엔티티 및 Dto 생성
    Entity
    - User (사용자 정보 엔티티)
    
    Dto
    - ResponseDto (message 응답을 위한 Dto)
    - NickNameCheckRequest (닉네임 중복체크 요청 dto)
    - EmailRequest (이메일 중복체크 및 이메일 전송 요청 dto)
    - CertificationRequest (인증번호 확인 요청 dto)
    - SignUpRequest (회원 가입 요청 dto)
    - SignInRequest, SignInResponse (로그인 요청, 응답 dto)
    - ReissueTokenRequest, ReissueTokenResponse (AccessToken 재발급 요청 , 응답 dto)
    - UserDto (유저 정보 응답 dto)

---
+++ src/main/java/com/devonoff/domain/user/dto/auth/ReissueTokenRequest.java
@@ -0,0 +1,25 @@
+package com.devonoff.domain.user.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReissueTokenRequest {
+
+  @Email
+  @NotBlank
+  private String email;
+
+  @NotBlank
+  private String refreshToken;
+
+}
