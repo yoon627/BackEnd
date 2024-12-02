f4d6dce02f87390f11919effcc769730eb370090
hjkim <hjhjgudwls1022@gmail.com>
2024-12-02 20:53:42 +0900

    fix: 매퍼 삭제 및 patch -> put으로 변경

--- src/main/java/com/devonoff/domain/studyPost/controller/StudyPostController.java
+++ src/main/java/com/devonoff/domain/studyPost/controller/StudyPostController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,7 +67,7 @@ public class StudyPostController {
   }
 
   // 스터디 모집글 수정
-  @PatchMapping("/{studyPostId}")
+  @PutMapping("/{studyPostId}")
   public ResponseEntity<StudyPostUpdateResponse> updateStudyPost(
       @PathVariable Long studyPostId, @RequestBody StudyPostUpdateRequest request) {
     StudyPostUpdateResponse response = studyPostService.updateStudyPost(studyPostId, request);
