12d8e2e7a7425d7fe940d7ae71b2e01f1e137318
yoon627 <science627@naver.com>
2024-12-03 14:32:37 +0900

    feat: 특정 스터디 타임라인을 조회할 권한이 있는지 확인하기 위해 repository에서 해당 스터디에 로그인된 유저가 속하는지 확인하는 메서드 추가

--- src/main/java/com/devonoff/domain/student/repository/StudentRepository.java
+++ src/main/java/com/devonoff/domain/student/repository/StudentRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+
+  Boolean existsByUserIdAndStudyId(Long userId, Long studyId);
 }
