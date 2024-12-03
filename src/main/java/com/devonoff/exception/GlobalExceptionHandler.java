4dd19f5c1f61962a80cda7e2e402c6a4b583372a
SwanyCastle <seunghwan712@gmail.com>
2024-12-03 20:16:40 +0900

    feat: API 요청시 Request Body 데이터 유효성 검사 MethodArgumentNotValidException 에러 처리 GlobalExceptionHandler 추가
    - 상태코드 400 에 메시지 "잘못된 요청입니다." 로 응답하도록 설정

--- src/main/java/com/devonoff/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devonoff/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.devonoff.exception;
 
+import static com.devonoff.type.ErrorCode.BAD_REQUEST;
 import static com.devonoff.type.ErrorCode.INTERNAL_SERVER_ERROR;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +18,13 @@ public class GlobalExceptionHandler {
   public ResponseEntity<String> handleCustomException(CustomException e) {
     log.error("{} is occurred", e.getErrorCode());
     return ResponseEntity.status(e.getErrorCode().getStatus()).body(e.getErrorMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleMethodArgumentNotValidException(Exception e) {
+    log.error("MethodArgumentNotValidException Error is occurred", e);
+    return ResponseEntity.status(BAD_REQUEST.getStatus())
+        .body(BAD_REQUEST.getDescription());
   }
 
   @ExceptionHandler(Exception.class)
