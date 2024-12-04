c5c946059e7604cfa0544656b6c7b824c26c9db8
yoon627 <science627@naver.com>
2024-12-04 18:18:48 +0900

    fix: totalstudytimeDto가 ranking,percent를 추가로 내려주도록 변경, totalStudyTime이 초단위인 Long에서 년,개월,일,시간,분,초로 바꿔서 String으로 바꿔주도록 변경

--- src/main/java/com/devonoff/domain/totalstudytime/service/TotalStudyTimeService.java
+++ src/main/java/com/devonoff/domain/totalstudytime/service/TotalStudyTimeService.java
@@ -4,9 +4,11 @@ import static com.devonoff.type.ErrorCode.STUDY_NOT_FOUND;
 
 import com.devonoff.domain.study.repository.StudyRepository;
 import com.devonoff.domain.totalstudytime.dto.TotalStudyTimeDto;
+import com.devonoff.domain.totalstudytime.entity.TotalStudyTime;
 import com.devonoff.domain.totalstudytime.repository.TotalStudyTimeRepository;
 import com.devonoff.exception.CustomException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,17 +23,38 @@ public class TotalStudyTimeService {
   public TotalStudyTimeDto getTotalStudyTime(Long studyId) {
     String studyName = this.studyRepository.findById(studyId)
         .orElseThrow(() -> new CustomException(STUDY_NOT_FOUND)).getStudyName();
-    return TotalStudyTimeDto.fromEntityWithStudyName(
+
+    List<TotalStudyTime> allTotalStudyTimes = this.totalStudyTimeRepository.findAllByOrderByTotalStudyTimeDesc();
+
+    AtomicLong rank = new AtomicLong(1);
+    Long studyRanking = allTotalStudyTimes.stream()
+        .map(entity -> {
+          if (entity.getStudyId().equals(studyId)) {
+            return rank.get();
+          }
+          rank.incrementAndGet();
+          return null;
+        })
+        .filter(java.util.Objects::nonNull) // Null 값 제외
+        .findFirst()
+        .orElseThrow(() -> new CustomException(STUDY_NOT_FOUND));
+
+    return TotalStudyTimeDto.fromEntityWithStudyNameAndRanking(
         this.totalStudyTimeRepository.findById(studyId).orElseThrow(() -> new CustomException(
-            STUDY_NOT_FOUND)), studyName);
+            STUDY_NOT_FOUND)), studyName, studyRanking,
+        (studyRanking.doubleValue() / allTotalStudyTimes.size()) * 100);
   }
 
   public List<TotalStudyTimeDto> getTotalStudyTimeRanking() {
+    Long totalStudyTimeCnt = this.totalStudyTimeRepository.count();
+    AtomicLong rankingCount = new AtomicLong(1);
     return this.totalStudyTimeRepository.findTop10ByOrderByTotalStudyTimeDesc().stream()
         .map(totalStudyTime -> {
-          String studyName = this.studyRepository.findById(totalStudyTime.getId())
+          String studyName = this.studyRepository.findById(totalStudyTime.getStudyId())
               .orElseThrow(() -> new CustomException(STUDY_NOT_FOUND)).getStudyName();
-          return TotalStudyTimeDto.fromEntityWithStudyName(totalStudyTime, studyName);
+          return TotalStudyTimeDto.fromEntityWithStudyNameAndRanking(totalStudyTime, studyName,
+              rankingCount.get(),
+              (rankingCount.getAndIncrement() / totalStudyTimeCnt.doubleValue()) * 100);
         }).collect(Collectors.toList());
   }
 }
