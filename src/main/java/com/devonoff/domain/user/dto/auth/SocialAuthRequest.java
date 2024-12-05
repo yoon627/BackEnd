28eba2493a26ca9129f42387796fe3645f98a3ca
SwanyCastle <seunghwan712@gmail.com>
2024-12-05 20:35:37 +0900

    fix: 테스트시 Mock 생성을 위해 SocialAuthRequest 빌더 패턴 적용 및 SocialAuthService 에서 try catch 로 예외 처리

--- src/main/java/com/devonoff/domain/user/dto/auth/SocialAuthRequest.java
+++ src/main/java/com/devonoff/domain/user/dto/auth/SocialAuthRequest.java
@@ -1,9 +1,15 @@
 package com.devonoff.domain.user.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SocialAuthRequest {
 
   @NotBlank
