e6bc7cbdc6a71b1e76ac9b475766d2da863de5a6
SwanyCastle <seunghwan712@gmail.com>
2024-12-06 18:44:28 +0900

    refactor: 안쓰는 import 정리

--- src/test/java/com/devonoff/domain/user/controller/AuthControllerTest.java
+++ src/test/java/com/devonoff/domain/user/controller/AuthControllerTest.java
@@ -32,7 +32,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AuthController.class)
