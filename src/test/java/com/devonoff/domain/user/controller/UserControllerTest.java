ddd0eb4fa917ba91c0b67f2036a81f1810b50e7f
SwanyCastle <seunghwan712@gmail.com>
2024-12-09 13:09:32 +0900

    fix: 코드 변경으로 인한 테스트 코드 수정
    - 프로필 이미지 삭제, 수정시 ModelAttribute -> RequestParam 으로 이미지 파일 받는 방식 변경으로 인한 테스트 코드 변경
    - 프로필 이미지 삭제시 프로필 이미지 null -> 프로필 이미지를 기본 이미지로 변경 로직에 따른 테스트 코드 변경
    - 회원 가입시 비밀번호 패턴 확인으로 인해 테스트 코드 변경

--- src/test/java/com/devonoff/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/devonoff/domain/user/controller/UserControllerTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,6 +35,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -204,11 +206,19 @@ class UserControllerTest {
         .profileImageUrl("testProfileImageUrl")
         .build();
 
+    MockMultipartFile file = new MockMultipartFile(
+        "profileImage",
+        "test.jpg",
+        MediaType.IMAGE_JPEG_VALUE,
+        "test image content".getBytes()
+    );
+
     given(userService.updateProfileImage(eq(1L), any(MultipartFile.class)))
         .willReturn(userDto);
 
     // when, then
-    mockMvc.perform(post("/api/users/1/profile-image")
+    mockMvc.perform(multipart("/api/users/1/profile-image")
+            .file(file)
             .contentType(MediaType.MULTIPART_FORM_DATA))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.id").value(userDto.getId()))
@@ -221,11 +231,19 @@ class UserControllerTest {
   @DisplayName("프로필 이미지 수정 - 실패 (로그인한 유저와 불일치)")
   void testUpdateProfileImage_Fail_UserUnMatched() throws Exception {
     // given
+    MockMultipartFile file = new MockMultipartFile(
+        "profileImage",
+        "test.jpg",
+        MediaType.IMAGE_JPEG_VALUE,
+        "test image content".getBytes()
+    );
+
     given(userService.updateProfileImage(eq(1L), any(MultipartFile.class)))
         .willThrow(new CustomException(ErrorCode.UNAUTHORIZED_USER));
 
     // when, then
-    mockMvc.perform(post("/api/users/1/profile-image")
+    mockMvc.perform(multipart("/api/users/1/profile-image")
+            .file(file)
             .contentType(MediaType.MULTIPART_FORM_DATA))
         .andExpect(status().isUnauthorized());
   }
@@ -234,11 +252,19 @@ class UserControllerTest {
   @DisplayName("프로필 이미지 수정 - 실패 (존재하지 않는 유저)")
   void testUpdateProfileImage_Fail_UserNotFound() throws Exception {
     // given
+    MockMultipartFile file = new MockMultipartFile(
+        "profileImage",
+        "test.jpg",
+        MediaType.IMAGE_JPEG_VALUE,
+        "test image content".getBytes()
+    );
+
     given(userService.updateProfileImage(eq(1L), any(MultipartFile.class)))
         .willThrow(new CustomException(ErrorCode.USER_NOT_FOUND));
 
     // when, then
-    mockMvc.perform(post("/api/users/1/profile-image")
+    mockMvc.perform(multipart("/api/users/1/profile-image")
+            .file(file)
             .contentType(MediaType.MULTIPART_FORM_DATA))
         .andExpect(status().isNotFound());
   }
@@ -260,12 +286,24 @@ class UserControllerTest {
   @DisplayName("프로필 이미지 삭제 - 성공")
   void testDeleteProfileImage_Success() throws Exception {
     // given
-    willDoNothing().given(userService).deleteProfileImage(eq(1L));
+    UserDto userDto = UserDto.builder()
+        .id(1L)
+        .nickname("newNickname")
+        .email("test@email.com")
+        .profileImageUrl("defaultProfileImageUrl")
+        .build();
+
+    given(userService.deleteProfileImage(eq(1L)))
+        .willReturn(userDto);
 
     // when, then
     mockMvc.perform(delete("/api/users/1/profile-image")
             .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(userDto.getId()))
+        .andExpect(jsonPath("$.nickname").value(userDto.getNickname()))
+        .andExpect(jsonPath("$.email").value(userDto.getEmail()))
+        .andExpect(jsonPath("$.profileImageUrl").value(userDto.getProfileImageUrl()));
   }
 
   @Test
