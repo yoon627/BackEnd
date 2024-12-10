bdcba96425c4704d417ee3faf019ac17c1c737c2
SwanyCastle <seunghwan712@gmail.com>
2024-12-10 19:25:57 +0900

    fix: 댓글, 대댓글 응답 값 api 명세서대로 통일 및 필요없는 코드 삭제
    - 코드 정리
    - RequestBody 와 ResponseBody 가 api 명세와 동일하도록 수정
    - CommentController 에서 PostType String 으로 받아서 Enum 으로 변환하는거 Enum 으로 받는걸로 변경
    - 댓글 삭제시 댓글에 달린 대댓글 삭제 추가

--- src/main/java/com/devonoff/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/devonoff/domain/comment/repository/CommentRepository.java
@@ -2,7 +2,6 @@ package com.devonoff.domain.comment.repository;
 
 import com.devonoff.domain.comment.entity.Comment;
 import com.devonoff.type.PostType;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -11,8 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-  List<Comment> findByPostIdAndPostType(Long postId, PostType postType);
-
   // 특정 게시글의 댓글과 대댓글을 함께 조회
   @EntityGraph(attributePaths = {"replies"})
   Page<Comment> findByPostIdAndPostType(Long postId, PostType postType, Pageable pageable);
--- src/main/java/com/devonoff/domain/reply/Repository/ReplyRepository.java
+++ src/main/java/com/devonoff/domain/reply/Repository/ReplyRepository.java
@@ -1,7 +1,7 @@
 package com.devonoff.domain.reply.Repository;
 
+import com.devonoff.domain.comment.entity.Comment;
 import com.devonoff.domain.reply.entity.Reply;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
-  List<Reply> findByCommentId(Long commentId);
+  void deleteAllByComment(Comment comment);
 }
 
