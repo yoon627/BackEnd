e1fe195a8a91025ea1598d4fd3feae2bc7d44490
hjkim <hjhjgudwls1022@gmail.com>
2024-12-23 18:46:50 +0900

    fix: 자동 취소 및 삭제 로직 수정 및 관련 엔티티 연관관계 개선

--- src/main/java/com/devonoff/config/BatchConfig.java
+++ src/main/java/com/devonoff/config/BatchConfig.java
@@ -1,10 +1,7 @@
 package com.devonoff.config;
 
-import com.devonoff.domain.studyPost.entity.StudyComment;
 import com.devonoff.domain.studyPost.entity.StudyPost;
-import com.devonoff.domain.studyPost.repository.StudyCommentRepository;
 import com.devonoff.domain.studyPost.repository.StudyPostRepository;
-import com.devonoff.domain.studyPost.repository.StudyReplyRepository;
 import com.devonoff.domain.studyPost.service.StudyPostService;
 import com.devonoff.type.StudyPostStatus;
 import com.devonoff.util.TimeProvider;
@@ -27,8 +24,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class BatchConfig {
 
   private final StudyPostRepository studyPostRepository;
-  private final StudyCommentRepository studyCommentRepository;
-  private final StudyReplyRepository studyReplyRepository;
   private final StudyPostService studyPostService;
   private final TimeProvider timeProvider;
 
@@ -53,24 +48,12 @@ public class BatchConfig {
     return (contribution, chunkContext) -> {
       LocalDateTime oneWeekAgo = timeProvider.now().minusDays(7);
 
-      // 모집 기한이 지난 스터디 모집글을 CANCELED 로 변경(배치작업으로 자동 취소)
+      // 모집 기한이 지난 스터디 모집글을 CANCELED 로 변경
       studyPostService.cancelStudyPostIfExpired();
 
+      // 취소 상태에서 일주일이 지난 스터디 모집글 삭제
       List<StudyPost> studyPostsToDelete = studyPostRepository.findAllByStatusAndUpdatedAtBefore(
           StudyPostStatus.CANCELED, oneWeekAgo);
-
-      // 댓글 / 대댓글 삭제
-      for (StudyPost studyPost : studyPostsToDelete) {
-        List<StudyComment> comments = studyCommentRepository.findAllByStudyPost(studyPost);
-
-        for (StudyComment comment : comments) {
-          studyReplyRepository.deleteAllByComment(comment);
-        }
-
-        studyCommentRepository.deleteAllByStudyPost(studyPost);
-      }
-
-      // 취소 상태에서 일주일이 지난 스터디 모집글 삭제
       studyPostRepository.deleteAll(studyPostsToDelete);
 
       return RepeatStatus.FINISHED;
