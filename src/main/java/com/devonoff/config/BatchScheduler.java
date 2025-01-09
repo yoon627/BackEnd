d1fe9c756709845102558138a3d3c4ed9ef77124
hjkim <hjhjgudwls1022@gmail.com>
2025-01-09 14:55:00 +0900

    chore: 스케줄러 활성화를 위한 @EnableScheduling 어노테이션 추가

--- src/main/java/com/devonoff/config/BatchScheduler.java
+++ src/main/java/com/devonoff/config/BatchScheduler.java
@@ -12,10 +12,14 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Configuration
+@EnableScheduling
 @RequiredArgsConstructor
 @Slf4j
 public class BatchScheduler {
